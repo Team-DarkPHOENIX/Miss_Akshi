5df5bf8e4a218c6ce8ba70ec6fefe9aa7a4a33a4
zeinzo <91390078+zeinzo@users.noreply.github.com>
2021-10-06 06:54:13 +0700

    Update config.py

--- lunaBot/config.py
+++ lunaBot/config.py
@@ -4,7 +4,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/innexia/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/luna/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
