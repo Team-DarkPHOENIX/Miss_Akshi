9084f437885aed63b63e115fcad7d81f0ffb7f33
zeinzo <91390078+zeinzo@users.noreply.github.com>
2021-10-06 07:20:41 +0700

    Update __main__.py

--- lunaBot/__main__.py
+++ lunaBot/__main__.py
@@ -397,7 +397,7 @@ def luna_about_callback(update, context):
             text=f"*Here's basic Help regarding* *How to use Me?*"
             f"\n\n• Firstly Add {dispatcher.bot.first_name} to your group by pressing [here](http://t.me/{dispatcher.bot.username}?startgroup=true)\n"
             f"\n• After adding promote me manually with full rights for faster experience.\n"
-            f"\n• Than send `/admincache@todorokiXrobot` in that chat to refresh admin list in My database.\n"
+            f"\n• Than send `/admincache@lunatapibot` in that chat to refresh admin list in My database.\n"
             f"\n\n*All done now use below given button's to know about use!*\n"
             f"",
             parse_mode=ParseMode.MARKDOWN,
