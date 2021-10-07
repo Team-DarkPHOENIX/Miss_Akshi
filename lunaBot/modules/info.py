55a05a4f99d578f21f9f70eca5047659e7a6d7e5
riz-ex <90839716+riz-ex@users.noreply.github.com>
2021-10-08 04:46:14 +0700

    Update info.py

--- lunaBot/modules/info.py
+++ lunaBot/modules/info.py
@@ -1,40 +1,10 @@
-"""
-MIT License
-
-Copyright (c) 2021 TheHamkerCat
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-import os
 
 from pyrogram import filters
 from pyrogram.types import Message
 
-from lunaBot import SUDOERS, app
+from lunaBot import DEV_USERS
+from lunaBot import pbot as app
 from lunaBot.core.sections import section
-from lunaBot.utils.dbfunctions import is_gbanned_user, user_global_karma
-
-__MODULE__ = "Info"
-__HELP__ = """
-/info [USERNAME|ID] - Get info about a user.
-/chat_info [USERNAME|ID] - Get info about a chat.
-"""
 
 
 async def get_user_info(user, already=False):
@@ -48,18 +18,14 @@ async def get_user_info(user, already=False):
     mention = user.mention("Link")
     dc_id = user.dc_id
     photo_id = user.photo.big_file_id if user.photo else None
-    is_gbanned = await is_gbanned_user(user_id)
-    is_sudo = user_id in SUDOERS
-    karma = await user_global_karma(user_id)
+    is_sudo = user_id in DEV_USERS
     body = {
         "ID": user_id,
         "DC": dc_id,
         "Name": [first_name],
-        "Username": [("@" + username) if username else "Null"],
+        "Username": [("@" + username) if username else None],
         "Mention": [mention],
         "Sudo": is_sudo,
-        "Karma": karma,
-        "Gbanned": is_gbanned,
     }
     caption = section("User info", body)
     return [caption, photo_id]
@@ -76,7 +42,7 @@ async def get_chat_info(chat, already=False):
     description = chat.description
     members = chat.members_count
     is_restricted = chat.is_restricted
-    link = f"[Link](t.me/{username})" if username else "Null"
+    link = f"[Link](t.me/{username})" if username else None
     dc_id = chat.dc_id
     photo_id = chat.photo.big_file_id if chat.photo else None
     body = {
@@ -84,7 +50,7 @@ async def get_chat_info(chat, already=False):
         "DC": dc_id,
         "Type": type_,
         "Name": [title],
-        "Username": [("@" + username) if username else "Null"],
+        "Username": [("@" + username) if username else None],
         "Mention": [link],
         "Members": members,
         "Scam": is_scam,
@@ -95,7 +61,7 @@ async def get_chat_info(chat, already=False):
     return [caption, photo_id]
 
 
-@app.on_message(filters.command("info"))
+@app.on_message(filters.command("uinfo"))
 async def info_func(_, message: Message):
     if message.reply_to_message:
         user = message.reply_to_message.from_user.id
@@ -104,7 +70,7 @@ async def info_func(_, message: Message):
     elif not message.reply_to_message and len(message.command) != 1:
         user = message.text.split(None, 1)[1]
 
-    m = await message.reply_text("Processing")
+    m = await message.reply_text("Processing...")
 
     try:
         info_caption, photo_id = await get_user_info(user)
@@ -112,20 +78,24 @@ async def info_func(_, message: Message):
         return await m.edit(str(e))
 
     if not photo_id:
-        return await m.edit(info_caption, disable_web_page_preview=True)
+        return await m.edit(
+            info_caption, disable_web_page_preview=True
+        )
     photo = await app.download_media(photo_id)
 
-    await message.reply_photo(photo, caption=info_caption, quote=False)
+    await message.reply_photo(
+        photo, caption=info_caption, quote=False
+    )
     await m.delete()
     os.remove(photo)
 
 
-@app.on_message(filters.command("chat_info"))
+@app.on_message(filters.command("cinfo"))
 async def chat_info_func(_, message: Message):
     try:
         if len(message.command) > 2:
             return await message.reply_text(
-                "**Usage:**/chat_info [USERNAME|ID]"
+                "**Usage:**cinfo <chat id/username>"
             )
 
         if len(message.command) == 1:
@@ -133,16 +103,23 @@ async def chat_info_func(_, message: Message):
         elif len(message.command) == 2:
             chat = message.text.split(None, 1)[1]
 
-        m = await message.reply_text("Processing")
+        m = await message.reply_text("Processing...")
 
         info_caption, photo_id = await get_chat_info(chat)
         if not photo_id:
-            return await m.edit(info_caption, disable_web_page_preview=True)
+            return await m.edit(
+                info_caption, disable_web_page_preview=True
+            )
 
         photo = await app.download_media(photo_id)
-        await message.reply_photo(photo, caption=info_caption, quote=False)
+        await message.reply_photo(
+            photo, caption=info_caption, quote=False
+        )
 
         await m.delete()
         os.remove(photo)
     except Exception as e:
         await m.edit(e)
+
+
+__mod_name__ = "SecInfo"
