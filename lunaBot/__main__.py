1bfdd91a168fbc4b9f9ee4d1a934b35cbbd4e099
zeinzo <91390078+zeinzo@users.noreply.github.com>
2021-10-29 17:41:28 +0700

    Update __main__.py

--- lunaBot/__main__.py
+++ lunaBot/__main__.py
@@ -453,19 +453,19 @@ def luna_about_callback(update, context):
         )
     elif query.data == "luna_support":
         query.message.edit_text(
-            text=f"──「 Admin CMD 」──"
-            f"/player - show the music playing status"
-            f"/pause - pause the music streaming"
-            f"/resume - resume the music was paused"
-            f"/skip - skip to the next song"
-            f"/end - stop music streaming"
-            f"/join - invite userbot join to your group"
-            f"/leave - order the userbot to leave your group"
-            f"/auth - authorized user for using music bot"
-            f"/unauth - unauthorized for using music bot"
-            f"/control - open the player settings panel"
-            f"/delcmd (on | off) - enable / disable del cmd feature"
-            f"/music (on / off) - disable / enable music player in your group",
+            text=f"──「 Admin CMD 」──\n"
+            f"\n/player - show the music playing status"
+            f"\n/pause - pause the music streaming"
+            f"\n/resume - resume the music was paused"
+            f"\n/skip - skip to the next song"
+            f"\n/end - stop music streaming"
+            f"\n/join - invite userbot join to your group"
+            f"\n/leave - order the userbot to leave your group"
+            f"\n/auth - authorized user for using music bot"
+            f"\n/unauth - unauthorized for using music bot"
+            f"\n/control - open the player settings panel"
+            f"\n/delcmd (on | off) - enable / disable del cmd feature"
+            f"\n/music (on / off) - disable / enable music player in your group",
             parse_mode=ParseMode.MARKDOWN,
             reply_markup=InlineKeyboardMarkup(
                 [
