6cbcdb3e9025e0a23a97fcda16e1d6adb568abb1
zeinzo <91390078+zeinzo@users.noreply.github.com>
2021-11-02 17:31:22 +0700

    Update stickers.py

--- lunaBot/modules/stickers.py
+++ lunaBot/modules/stickers.py
@@ -451,13 +451,6 @@ def makepack_internal(
         msg.reply_text("Failed to create sticker pack. Possibly due to blek mejik.")
 
 
-__help__ = """
- ❍ /stickerid*:* reply to a sticker to me to tell you its file ID.
- ❍ /getsticker*:* reply to a sticker to me to upload its raw PNG file.
- ❍ /kang*:* reply to a sticker to add it to your pack.
- ❍ /stickers*:* Find stickers for given term on combot sticker catalogue
-"""
-
 __mod_name__ = "sᴛɪᴄᴋᴇʀ"
 STICKERID_HANDLER = DisableAbleCommandHandler("stickerid", stickerid)
 GETSTICKER_HANDLER = DisableAbleCommandHandler("getsticker", getsticker)
