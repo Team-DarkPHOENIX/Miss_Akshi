3fc8b172c5c21c0e62bf178e7982e5bed64c5cf5
zeinzo <91390078+zeinzo@users.noreply.github.com>
2021-11-02 17:32:26 +0700

    Update __songs.py

--- lunaBot/modules/__songs.py
+++ lunaBot/modules/__songs.py
@@ -148,11 +148,3 @@ async def deezer(_, message):
     
 __mod_name__ = "ᴍᴜsɪᴄ"
 
-__help__ = """
-• `/song`** <songname artist(optional)>: download the song in it's best quality available.(API BASED)
-• `/video`** <songname artist(optional)>: download the video song in it's best quality available.
-• `/deezer`** <songname>: download from deezer
-• `/lyrics`** <songname artist(optional)>: sends the complete lyrics of the song provided as input
-• `/glyrics`** <i> song name </i> : This plugin searches for song lyrics with song name and artist.
-"""
-
