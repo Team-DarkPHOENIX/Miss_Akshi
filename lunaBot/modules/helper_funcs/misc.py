c6a1e108c4dd5d5dccb986e4e3168b1a27b34c18
zeinzo <91390078+zeinzo@users.noreply.github.com>
2021-11-02 17:05:10 +0700

    Update misc.py

--- lunaBot/modules/helper_funcs/misc.py
+++ lunaBot/modules/helper_funcs/misc.py
@@ -66,9 +66,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     # can only have a certain amount of buttons side by side
     if len(pairs) > 8:
         pairs = pairs[modulo_page * 8:8 * (modulo_page + 1)] + [
-            (EqInlineKeyboardButton("🔻", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("ʜᴏᴍᴇ", callback_data="luna_back"),
-             EqInlineKeyboardButton("🔺", callback_data="{}_next({})".format(prefix, modulo_page)))]
+            (EqInlineKeyboardButton("ʜᴏᴍᴇ", callback_data="luna_back"))]
 
     else:
         pairs += [[EqInlineKeyboardButton("ʙᴀᴄᴋ", callback_data="luna_back")]]
