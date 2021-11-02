b67f9bdac4f54c9fb39811dcb8c77d1573a5664e
zeinzo <91390078+zeinzo@users.noreply.github.com>
2021-11-02 17:33:26 +0700

    Update reporting.py

--- lunaBot/modules/reporting.py
+++ lunaBot/modules/reporting.py
@@ -263,16 +263,6 @@ def buttons(update: Update, context: CallbackContext):
             query.answer("🛑 Failed to delete message!")
 
 
-__help__ = """
- ❍ /report <reason>*:* reply to a message to report it to admins.
- ❍ @admin*:* reply to a message to report it to admins.
-*NOTE:* Neither of these will get triggered if used by admins.
-
-*Admins only:*
- ❍ /reports <on/off>*:* change report setting, or view current status.
-   • If done in pm, toggles your status.
-   • If in group, toggles that groups's status.
-"""
 
 SETTING_HANDLER = CommandHandler("reports", report_setting)
 REPORT_HANDLER = CommandHandler("report", report, filters=Filters.group)
