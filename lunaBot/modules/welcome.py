212a7de72126fcdeddb663a38e35b02e78c732e6
zeinzo <91390078+zeinzo@users.noreply.github.com>
2021-11-12 16:59:58 +0700

    Update welcome.py

--- lunaBot/modules/welcome.py
+++ lunaBot/modules/welcome.py
@@ -190,7 +190,7 @@ def new_member(update: Update, context: CallbackContext):
             # Give the owner a special welcome
             if new_mem.id == OWNER_ID:
                 update.effective_message.reply_text(
-                    "woahh tuanku akhirnya datang untung menemaniku🥳\n\n• @lunasupportgroup.", reply_to_message_id=reply
+                    "WOW __Lord Fizi__ Telah Bergabung.\n\n• @fantasyvirtual", reply_to_message_id=reply
                 )
                 welcome_log = (
                     f"{html.escape(chat.title)}\n"
