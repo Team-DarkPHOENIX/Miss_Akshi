dc17356d79c222f34c8e2c6bc61510989f238428
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-11-28 15:01:53 +0700

    Ok

--- lunaBot/events.py
+++ lunaBot/events.py
@@ -1,4 +1,5 @@
 from telethon import events
+
 from lunaBot import telethn
 
 
@@ -18,48 +19,3 @@ def register(**args):
         return func
 
     return decorator
-
-
-def chataction(**args):
-    """ Registers chat actions. """
-
-    def decorator(func):
-        telethn.add_event_handler(func, events.ChatAction(**args))
-        return func
-
-    return decorator
-
-
-def userupdate(**args):
-    """ Registers user updates. """
-
-    def decorator(func):
-        telethn.add_event_handler(func, events.UserUpdate(**args))
-        return func
-
-    return decorator
-
-
-def inlinequery(**args):
-    """ Registers inline query. """
-    pattern = args.get("pattern", None)
-
-    if pattern is not None and not pattern.startswith("(?i)"):
-        args["pattern"] = "(?i)" + pattern
-
-    def decorator(func):
-        telethn.add_event_handler(func, events.InlineQuery(**args))
-        return func
-
-    return decorator
-
-
-def callbackquery(**args):
-    """ Registers inline query. """
-
-    def decorator(func):
-        telethn.add_event_handler(func, events.CallbackQuery(**args))
-        return func
-
-    return decorator
-
