b0fdfaf8a64de49ad0554c7b7c0d77774b740e39
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-12-02 22:22:56 +0700

    Fixed

--- lunaBot/modules/music.py
+++ lunaBot/modules/music.py
@@ -1,23 +1,27 @@
 from __future__ import unicode_literals
 
 import os
+import wget
+import time
+import yt_dlp
+import asyncio
+import aiohttp
+import requests
+import ffmpeg
+
+from pyrogram import filters
+from pyrogram.types import Message
+from lunaBot import BOT_USERNAME, aiohttpsession as session, pbot as app, pgram
+from lunaBot.utils.pluginhelp import get_text, progress
+from lunaBot.modules.arq_client import arq
+from lunaBot.utils.errors import capture_err
+from lunaBot.utils.pastebin import epaste
+from youtube_search import YoutubeSearch
+from youtubesearchpython import SearchVideos
 from asyncio import get_running_loop
 from functools import partial
 from io import BytesIO
 from urllib.parse import urlparse
-
-import ffmpeg
-import youtube_dl
-from pyrogram import filters
-
-from lunaBot import aiohttpsession as session
-from lunaBot.modules.memek import arq
-from lunaBot import pbot as app
-from lunaBot.utils.errors import capture_err
-from lunaBot.utils.pastebin import epaste, hpaste
-
-__mod_name__ = "Media"
-
 
 is_downloading = False
 
@@ -30,7 +34,7 @@ def get_file_extension_from_url(url):
 
 def download_youtube_audio(url: str):
     global is_downloading
-    with youtube_dl.YoutubeDL(
+    with yt_dlp.YoutubeDL(
         {
             "format": "bestaudio",
             "writethumbnail": True,
@@ -52,15 +56,141 @@ def download_youtube_audio(url: str):
             os.remove(audio_file)
             audio_file = audio_file_opus
         thumbnail_url = info_dict["thumbnail"]
-        thumbnail_file = (
-            basename
-            + "."
-            + get_file_extension_from_url(thumbnail_url)
-        )
+        thumbnail_file = basename + "." + get_file_extension_from_url(thumbnail_url)
         title = info_dict["title"]
         performer = info_dict["uploader"]
         duration = int(float(info_dict["duration"]))
     return [title, performer, duration, audio_file, thumbnail_file]
+
+
+def time_to_seconds(time):
+    stringt = str(time)
+    return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(":"))))
+
+
+@pgram.on_message(filters.command(["vsong", "video"]))
+async def ytmusic(client, message: Message):
+    urlissed = get_text(message)
+    pablo = await client.send_message(
+        message.chat.id, f"Name ➛ {urlissed} 🔎 Finding the song..."
+    )
+    if not urlissed:
+        await pablo.edit("Invalid Command Syntax")
+        return
+    search = SearchVideos(f"{urlissed}", offset=1, mode="dict", max_results=1)
+    mi = search.result()
+    mio = mi["search_result"]
+    mo = mio[0]["link"]
+    thum = mio[0]["title"]
+    fridayz = mio[0]["id"]
+    thums = mio[0]["channel"]
+    kekme = f"https://img.youtube.com/vi/{fridayz}/hqdefault.jpg"
+    await asyncio.sleep(0.6)
+    url = mo
+    sedlyf = wget.download(kekme)
+    opts = {
+        "format": "best",
+        "addmetadata": True,
+        "key": "FFmpegMetadata",
+        "prefer_ffmpeg": True,
+        "geo_bypass": True,
+        "nocheckcertificate": True,
+        "postprocessors": [{"key": "FFmpegVideoConvertor", "preferedformat": "mp4"}],
+        "outtmpl": "%(id)s.mp4",
+        "logtostderr": False,
+        "quiet": True,
+    }
+    try:
+        with yt_dlp.YoutubeDL(opts) as ytdl:
+            ytdl_data = ytdl.extract_info(url, download=True)
+    except Exception as e:
+        await event.edit(event, f"**Failed To Download** \n**Error :** `{str(e)}`")
+        return
+    c_time = time.time()
+    file_stark = f"{ytdl_data['id']}.mp4"
+    capy = f"**Video Name ➠** `{thum}` \n**Requested For :** `{urlissed}` \n**Channel :** `{thums}`\n\nBy @{BOT_USERNAME}"
+    await client.send_video(
+        message.chat.id,
+        video=open(file_stark, "rb"),
+        duration=int(ytdl_data["duration"]),
+        file_name=str(ytdl_data["title"]),
+        thumb=sedlyf,
+        caption=capy,
+        supports_streaming=True,
+        progress=progress,
+        progress_args=(
+            pablo,
+            c_time,
+            f"`Uploading {urlissed} Song From YouTube Music!`",
+            file_stark,
+        ),
+    )
+    await pablo.delete()
+    for files in (sedlyf, file_stark):
+        if files and os.path.exists(files):
+            os.remove(files)
+
+
+@pgram.on_message(filters.command(["song", f"song@{BOT_USERNAME}"]))
+def song(client, message):
+
+    user_id = message.from_user.id
+    user_name = message.from_user.first_name
+    rpk = "[" + user_name + "](tg://user?id=" + str(user_id) + ")"
+
+    query = "".join(" " + str(i) for i in message.command[1:])
+    print(query)
+    m = message.reply("🔎 Finding the song...")
+    ydl_opts = {"format": "bestaudio[ext=m4a]"}
+    try:
+        results = YoutubeSearch(query, max_results=1).to_dict()
+        link = f"https://youtube.com{results[0]['url_suffix']}"
+        # print(results)
+        title = results[0]["title"][:40]
+        thumbnail = results[0]["thumbnails"][0]
+        thumb_name = f"thumb{title}.jpg"
+        thumb = requests.get(thumbnail, allow_redirects=True)
+        open(thumb_name, "wb").write(thumb.content)
+
+        duration = results[0]["duration"]
+        url_suffix = results[0]["url_suffix"]
+        views = results[0]["views"]
+
+    except Exception as e:
+        m.edit(
+            "✖️ Found Nothing. Sorry.\n\nTry another keywork or maybe spell it properly."
+        )
+        print(str(e))
+        return
+    m.edit("`Downloading Song... Please wait ⏱`")
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(link, download=False)
+            audio_file = ydl.prepare_filename(info_dict)
+            ydl.process_info(info_dict)
+        rep = f"🎙 **Title**: [{title[:35]}]({link})\n🎬 **Source**: YouTube\n⏱️ **Duration**: `{duration}`\n👁‍🗨 **Views**: `{views}`\n📤 **By**: @{BOT_USERNAME}"
+        secmul, dur, dur_arr = 1, 0, duration.split(":")
+        for i in range(len(dur_arr) - 1, -1, -1):
+            dur += int(dur_arr[i]) * secmul
+            secmul *= 60
+        message.reply_audio(
+            audio_file,
+            caption=rep,
+            thumb=thumb_name,
+            parse_mode="md",
+            title=title,
+            duration=dur,
+        )
+        m.delete()
+    except Exception as e:
+        m.edit("❌ Error")
+        print(e)
+
+    try:
+        os.remove(audio_file)
+        os.remove(thumb_name)
+    except Exception as e:
+        print(e)
 
 
 @app.on_message(filters.command("ytmusic"))
@@ -68,23 +198,17 @@ def download_youtube_audio(url: str):
 async def music(_, message):
     global is_downloading
     if len(message.command) != 2:
-        return await message.reply_text(
-            "/ytmusic needs a link as argument"
-        )
+        return await message.reply_text("/ytmusic needs a link as argument")
     url = message.text.split(None, 1)[1]
     if is_downloading:
         return await message.reply_text(
             "Another download is in progress, try again after sometime."
         )
     is_downloading = True
-    m = await message.reply_text(
-        f"Downloading {url}", disable_web_page_preview=True
-    )
+    m = await message.reply_text(f"Downloading {url}", disable_web_page_preview=True)
     try:
         loop = get_running_loop()
-        music = await loop.run_in_executor(
-            None, partial(download_youtube_audio, url)
-        )
+        music = await loop.run_in_executor(None, partial(download_youtube_audio, url))
         if not music:
             await m.edit("Too Long, Can't Download.")
         (
@@ -127,9 +251,7 @@ async def download_song(url):
 async def jssong(_, message):
     global is_downloading
     if len(message.command) < 2:
-        return await message.reply_text(
-            "/saavn requires an argument."
-        )
+        return await message.reply_text("/saavn requires an argument.")
     if is_downloading:
         return await message.reply_text(
             "Another download is in progress, try again after sometime."
@@ -162,20 +284,3 @@ async def jssong(_, message):
         return await m.edit(str(e))
     is_downloading = False
     song.close()
-
-
-# Lyrics
-
-
-@app.on_message(filters.command("lyricz"))
-async def lyrics_func(_, message):
-    if len(message.command) < 2:
-        return await message.reply_text("**Usage:**\n/lyrics [QUERY]")
-    m = await message.reply_text("**Searching**")
-    query = message.text.strip().split(None, 1)[1]
-    song = await arq.lyrics(query)
-    lyrics = song.result
-    if len(lyrics) < 4095:
-        return await m.edit(f"__{lyrics}__")
-    lyrics = await paste(lyrics)
-    await m.edit(f"**LYRICS_TOO_LONG:** [URL]({lyrics})")
