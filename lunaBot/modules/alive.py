ff8b8408471662a77c20e8122909482a85cc1c32
dark phoenix <79581828+darkphoenix2601@users.noreply.github.com>
2021-12-13 10:32:41 +0530

    Update alive.py

--- lunaBot/modules/alive.py
+++ lunaBot/modules/alive.py
@@ -1,29 +1,16 @@
-import re
-import os
-
-from telethon import events, Button
-from telethon import __version__ as tlhver
-from pyrogram import __version__ as pyrover
-from lunaBot.events import register as MEMEK
+from telethon import events, Button, custom
+import re, os
+from lunaBot.events import register
 from lunaBot import telethn as tbot
-
-PHOTO = "https://telegra.ph/file/7c3c26e0ed938aec91209.jpg"
-
-@MEMEK(pattern=("/alive"))
+from lunaBot import telethn as tgbot
+PHOTO = "https://telegra.ph/file/0d239215b8e4382f970ab.mp4"
+@register(pattern=("/alive"))
 async def awake(event):
-  tai = event.sender.first_name
-  LUNA = "**Holla I'm Luna!** \n\n"
-  LUNA += "🔴 **I'm Working Properly** \n\n"
-  LUNA += "🔴 **My Master : [zeinzo](https://t.me/tdrki_1)** \n\n"
-  LUNA += f"🔴 **Telethon Version : {tlhver}** \n\n"
-  LUNA += f"🔴 **Pyrogram Version : {pyrover}** \n\n"
-  LUNA += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/lunatapibot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/lunaXresso")]]
-  await tbot.send_file(event.chat_id, PHOTO, caption=LUNA,  buttons=BUTTON)
-
-@MEMEK(pattern=("/reload"))
-async def reload(event):
-  tai = event.sender.first_name
-  LUNA = "✅ **bot restarted successfully**\n\n• Admin list has been **updated**"
-  BUTTON = [[Button.url("📡 ᴜᴘᴅᴀᴛᴇs", "https://t.me/zeinproject")]]
-  await tbot.send_file(event.chat_id, PHOTO, caption=LUNA,  buttons=BUTTON)
+ Miss_Akshi = event.sender.first_name
+ Miss_Akshi = "♡ I,m Miss_Akshi💕 \n\n"
+ Miss_Akshi += "♡ I'm Working with awesome speed**\n\n"
+ Miss_Akshi += "**♡Miss_Akshi : 1.0 LATEST**\n\n"
+ Miss_Akshi += "**♡ 𝘔𝘺 𝘰𝘸𝘯𝘦𝘳 : [𝘈𝘴𝘩𝘶👑](t.me/Professer_Ashu)\n\n"
+ Miss_Akshi += "**♡ Telethon Version : 1.23.0**\n\n"
+ BUTTON = [[Button.url("𝘚𝘶𝘱𝘱𝘰𝘳𝘵🔥", "https://t.me/Miss_AkshiV1_Support"), Button.url("𝘜𝘱𝘥𝘢𝘵𝘦𝘴", "https://t.me/Miss_AkshiV1_Updates")]]
+ await tbot.send_file(event.chat_id, PHOTO, caption=Miss_Akshi,  buttons=BUTTON)
