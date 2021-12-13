9dd040b0b041161270a2ceff5758ed43397cfc0b
dark phoenix <79581828+darkphoenix2601@users.noreply.github.com>
2021-12-13 12:56:05 +0530

    Update __forcesubs.py

--- lunaBot/modules/__forcesubs.py
+++ lunaBot/modules/__forcesubs.py
@@ -181,7 +181,7 @@ def config(client, message):
 
 __help__ = """
 *Force Subscribe:*
-❍ Luna can mute members who are not subscribed your channel until they subscribe
+❍ Akshi can mute members who are not subscribed your channel until they subscribe
 ❍ When enabled I will mute unsubscribed members and show them a unmute button. When they pressed the button I will unmute them
 *Setup*
 *Only creator*
@@ -196,4 +196,4 @@ __help__ = """
   💡If you disable fsub, you need to set again for working.. /fsub {channel username} 
  ❍ /fsub clear - To unmute all members who muted by me.
 """
-__mod_name__ = "F-Subs"
+__mod_name__ = "F-Sub🪄"
