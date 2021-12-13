d192f37b00c6f655eb8205ddb7bdb9dc933c2a6e
dark phoenix <79581828+darkphoenix2601@users.noreply.github.com>
2021-12-13 10:20:45 +0530

    Update Chatbot.py

--- lunaBot/modules/Chatbot.py
+++ lunaBot/modules/Chatbot.py
@@ -353,12 +353,3 @@ async def inuka(client, message):
     except CFError:
         return
 
-
-__help__ = """
-• Luna is the only ai system which can detect & reply upto 200 language's
-
-✪ /chatbot ON/OFF: Enables and disables AI Chat mode.
-✪ /chatbot EN : Enables English only chatbot.
-"""
-
-__mod_name__ = "Chatbot"
