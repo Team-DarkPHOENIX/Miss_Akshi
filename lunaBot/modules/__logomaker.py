40da2bafd3b9ebab119ddeb1bc13827ebaa6a356
dark phoenix <79581828+darkphoenix2601@users.noreply.github.com>
2021-12-13 13:10:53 +0530

    Update __logomaker.py

--- lunaBot/modules/__logomaker.py
+++ lunaBot/modules/__logomaker.py
@@ -276,9 +276,9 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="white", stroke_width=1, stroke_fill="black")
     fname = "luna.png"
     img.save(fname, "png")
-    await telethn.send_file(event.chat_id, file=fname, caption = f"Made by [Luna ✨](https://t.me/lunatapibot)")         
+    await telethn.send_file(event.chat_id, file=fname, caption = f"Made by [Akshi ✨](@Miss_AkshiV1_Bot)")         
     await pesan.delete()
     if os.path.exists(fname):
             os.remove(fname)
  except Exception as e:
-    await event.reply(f'Error, Report @lunaXresso')
+    await event.reply(f'Error, Report @Miss_AkshiV1_Support')
