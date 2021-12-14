3a915a612bc8f3bd941cb262edaa6d5cc3aefdd4
dark phoenix <79581828+darkphoenix2601@users.noreply.github.com>
2021-12-14 21:01:02 +0530

    Update backups.py

--- lunaBot/modules/backups.py
+++ lunaBot/modules/backups.py
@@ -367,7 +367,7 @@ def get_chat(chat_id, chat_data):
         return {"status": False, "value": False}
 
 
-__mod_name__ = "Backup"
+__mod_name__ = "Backup📥"
 
 __help__ = """
 *Only for group owner:*
