61731f29502354346bef5d35aaa08d759bbca032
dark phoenix <79581828+darkphoenix2601@users.noreply.github.com>
2021-12-14 20:55:24 +0530

    Update admin.py

--- lunaBot/modules/admin.py
+++ lunaBot/modules/admin.py
@@ -637,31 +637,28 @@ def adminlist(update, context):
 
 
 __help__ = """
- ❍ /admins*:* list of admins in the chat
-
+  /admins*:* list of admins in the chat
 *Admins only:*
- ❍ /pin*:* silently pins the message replied to - add `'loud'` or `'notify'` to give notifs to users
- ❍ /unpin*:* unpins the currently pinned message
- ❍ /invitelink*:* gets invitelink
- ❍ /promote*:* promotes the user
- ❍ /demote*:* demotes the user
- ❍ /title <title here>*:* sets a custom title for an admin that the bot promoted
- ❍ /setgtitle <newtitle>*:* Sets new chat title in your group.
- ❍ /setgpic*:* As a reply to file or photo to set group profile pic!
- ❍ /delgpic*:* Same as above but to remove group profile pic.
- ❍ /setsticker*:* As a reply to some sticker to set it as group sticker set!
- ❍ /setdescription <description>*:* Sets new chat description in group.
- ❍ /admincache*:* force refresh the admins list
- ❍ /antispam <on/off/yes/no>*:* Will toggle our antispam tech or return your current settings.
- ❍ /del*:* deletes the message you replied to
- ❍ /purge*:* deletes all messages between this and the replied to message.
- ❍ /purge <integer X>*:* deletes the replied message, and X messages following it if replied to a message.
- ❍ /zombies: counts the number of deleted account in your group
- ❍ /zombies clean: Remove deleted accounts from group..
-
+  /pin*:* silently pins the message replied to - add `'loud'` or `'notify'` to give notifs to users
+  /unpin*:* unpins the currently pinned message
+  /invitelink*:* gets invitelink
+  /promote*:* promotes the user
+  /demote*:* demotes the user
+  /title <title here>*:* sets a custom title for an admin that the bot promoted
+  /setgtitle <newtitle>*:* Sets new chat title in your group.
+  /setgpic*:* As a reply to file or photo to set group profile pic!
+  /delgpic*:* Same as above but to remove group profile pic.
+  /setsticker*:* As a reply to some sticker to set it as group sticker set!
+  /setdescription <description>*:* Sets new chat description in group.
+  /admincache*:* force refresh the admins list
+  /antispam <on/off/yes/no>*:* Will toggle our antispam tech or return your current settings.
+  /del*:* deletes the message you replied to
+  /purge*:* deletes all messages between this and the replied to message.
+  /purge <integer X>*:* deletes the replied message, and X messages following it if replied to a message.
+  /zombies: counts the number of deleted account in your group
+  /zombies clean: Remove deleted accounts from group..
 *Note:* Night Mode chats get Automatically closed at 12 am(IST)
 and Automatically openned at 6 am(IST) To Prevent Night Spams.
-
 ⚠️ `Read from top`
 """
 
@@ -707,7 +704,7 @@ dispatcher.add_handler(SETCHAT_TITLE_HANDLER)
 dispatcher.add_handler(SETSTICKET_HANDLER)
 dispatcher.add_handler(SETDESC_HANDLER)
 
-__mod_name__ = "Admin"
+__mod_name__ = "Admin✨"
 __command_list__ = [
     "adminlist",
     "admins",
