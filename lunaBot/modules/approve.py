5b34d2b3ec3e2bd8c6f93ebcfc08d090669da24a
dark phoenix <79581828+darkphoenix2601@users.noreply.github.com>
2021-12-14 20:58:34 +0530

    Update approve.py

--- lunaBot/modules/approve.py
+++ lunaBot/modules/approve.py
@@ -197,6 +197,18 @@ def unapproveall_btn(update: Update, context: CallbackContext):
 
 
 
+__help__ = """
+Sometimes, you might trust a user not to send unwanted content.
+Maybe not enough to make them admin, but you might be ok with locks, blacklists, and antiflood not applying to them.
+That's what approvals are for - approve of trustworthy users to allow them to send 
+*Admin commands:*
+- `/approval`*:* Check a user's approval status in this chat.
+- `/approve`*:* Approve of a user. Locks, blacklists, and antiflood won't apply to them anymore.
+- `/unapprove`*:* Unapprove of a user. They will now be subject to locks, blacklists, and antiflood again.
+- `/approved`*:* List all approved users.
+- `/unapproveall`*:* Unapprove *ALL* users in a chat. This cannot be undone.
+"""
+
 APPROVE = DisableAbleCommandHandler("approve", approve)
 DISAPPROVE = DisableAbleCommandHandler("unapprove", disapprove)
 APPROVED = DisableAbleCommandHandler("approved", approved)
@@ -211,6 +223,6 @@ dispatcher.add_handler(APPROVAL)
 dispatcher.add_handler(UNAPPROVEALL)
 dispatcher.add_handler(UNAPPROVEALL_BTN)
 
-__mod_name__ = "ᴀᴘᴘʀᴏᴠᴇ"
+__mod_name__ = "Approvals👍"
 __command_list__ = ["approve", "unapprove", "approved", "approval"]
 __handlers__ = [APPROVE, DISAPPROVE, APPROVED, APPROVAL]
