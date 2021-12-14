f8c01a5eb6e1b4b373500c681512de7dcf2b4ab5
dark phoenix <79581828+darkphoenix2601@users.noreply.github.com>
2021-12-14 21:10:28 +0530

    Update cricketscore.py

--- lunaBot/modules/cricketscore.py
+++ lunaBot/modules/cricketscore.py
@@ -39,3 +39,11 @@ async def _(event):
         f"<b><u>Match information gathered successful</b></u>\n\n\n<code>{Sed}</code>",
         parse_mode="HTML",
     )
+
+    
+    __help__ = """
+*live cricket score*
+ ❍ /cs*:* Latest live scores from cricinfo
+"""
+
+__mod_name__ = "Cʀɪᴄᴋᴇᴛ🏏"
