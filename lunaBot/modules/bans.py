57bd81c7fb1d44f9bc63a9377c3087fefa5ef50b
dark phoenix <79581828+darkphoenix2601@users.noreply.github.com>
2021-12-14 21:02:33 +0530

    Update bans.py

--- lunaBot/modules/bans.py
+++ lunaBot/modules/bans.py
@@ -428,7 +428,7 @@ dispatcher.add_handler(UNBAN_HANDLER)
 dispatcher.add_handler(ROAR_HANDLER)
 dispatcher.add_handler(PUNCHME_HANDLER)
 
-__mod_name__ = "Restrictions"
+__mod_name__ = "Ban/Mute🚫"
 __handlers__ = [
     BAN_HANDLER,
     TEMPBAN_HANDLER,
