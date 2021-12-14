58bd877e700d727999b8bcc3fae877e4a3859e88
dark phoenix <79581828+darkphoenix2601@users.noreply.github.com>
2021-12-14 21:18:21 +0530

    Update gtranslator.py

--- lunaBot/modules/gtranslator.py
+++ lunaBot/modules/gtranslator.py
@@ -74,3 +74,19 @@ dispatcher.add_handler(LANGUAGE_HANDLER)
 
 
 __command_list__ = ["tr", "tl"]
+
+__help__ = """
+ ➢ /tr or /tl (language code) as reply to a long message
+*Example:* 
+ ➢ /tr en*:* translates something to english
+ ➢ /tr hi-en*:* translates hindi to english
+*Language Codes*
+`af,am,ar,az,be,bg,bn,bs,ca,ceb,co,cs,cy,da,de,el,en,eo,es,
+et,eu,fa,fi,fr,fy,ga,gd,gl,gu,ha,haw,hi,hmn,hr,ht,hu,hy,
+id,ig,is,it,iw,ja,jw,ka,kk,km,kn,ko,ku,ky,la,lb,lo,lt,lv,mg,mi,mk,
+ml,mn,mr,ms,mt,my,ne,nl,no,ny,pa,pl,ps,pt,ro,ru,sd,si,sk,sl,
+sm,sn,so,sq,sr,st,su,sv,sw,ta,te,tg,th,tl,tr,uk,ur,uz,
+vi,xh,yi,yo,zh,zh_CN,zh_TW,zu`
+"""
+
+__mod_name__ = "ɢ-ᴛʀᴀɴꜱ✍"
