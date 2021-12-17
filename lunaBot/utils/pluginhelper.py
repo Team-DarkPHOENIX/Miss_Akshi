28883f358aa9386718e1c3775fe71e1f5f0f1e96
dark phoenix <79581828+darkphoenix2601@users.noreply.github.com>
2021-12-17 14:12:29 +0530

    Update pluginhelper.py

--- lunaBot/utils/pluginhelper.py
+++ lunaBot/utils/pluginhelper.py
@@ -13,8 +13,8 @@ from pyrogram import Client
 from pyrogram.errors import FloodWait, MessageNotModified
 from pyrogram.types import Chat, Message, User
 
-from EmikoRobot import OWNER_ID, SUPPORT_CHAT
-from EmikoRobot import pbot
+from lunaBot import OWNER_ID, SUPPORT_CHAT
+from lunaBot import pbot
 
 
 def get_user(message: Message, text: str) -> [int, str, None]:
